e4f228766c351cf964c23615ca5f8b8deecf27d1
Christopher Sesugh <christophersesugh@Christophers-MacBook-Air.local>
2023-08-16 14:53:17 +0100

    fix: update symbolic config manager

--- symbol/symbolic-config-manager.js
+++ symbol/symbolic-config-manager.js
@@ -1,15 +1,22 @@
 function ConfigMngr() {
   this.configurations = new Map();
+  this.keyMap = new Map(); // Map to store the mapping between Symbol keys and original keys
 }
 
 ConfigMngr.prototype.set = function (key, value) {
-  const consfigSym = Symbol(key);
-  this.configurations.set(consfigSym, value);
+  const configSym = Symbol(key);
+  this.configurations.set(configSym, value);
+  this.keyMap.set(configSym, key); // Store the mapping between Symbol key and original key
 };
 
 ConfigMngr.prototype.get = function (key) {
-  const configSym = Symbol(key);
-  return this.configurations.get(configSym);
+  const configSym = [...this.keyMap.keys()].find(
+    (sym) => this.keyMap.get(sym) === key
+  );
+  if (configSym) {
+    return this.configurations.get(configSym);
+  }
+  return undefined; // Return undefined if the key isn't found
 };
 
 const testCM = new ConfigMngr();

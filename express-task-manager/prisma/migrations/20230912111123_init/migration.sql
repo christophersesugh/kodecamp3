48bb6823c5963c7b6b6dd287ab00734ba4171bc3
Christopher Sesugh <christophersesugh@Christophers-MacBook-Air.local>
2023-09-14 12:40:45 +0100

    fix

--- express-task-manager/prisma/migrations/20230912111123_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL DEFAULT false
-);
